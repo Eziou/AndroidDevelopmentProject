ecff1c6c1a25f7016ce75d25c3f9d39fb5ea1f3b
fangming zou <fangming.zou@edu.esiee.fr>
2021-03-26 23:49:45 +0100

    Edit MainActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,15 +2,62 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
 
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.new_reminder:{
+                Intent intent=new Intent(this,ReminderAddActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            case R.id.search: {
+                Toast.makeText(this, "Search", Toast.LENGTH_LONG).show();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public VerticalItemHolder onCreateViewHolder(ViewGroup container, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(container.getContext());
+        View root = inflater.inflate(R.layout.recycle, container, false);
+
+        return new VerticalItemHolder(root, this);
+    }
+
 }
--- app/src/main/java/com/example/project/ReminderAddActivity.java
+++ app/src/main/java/com/example/project/ReminderAddActivity.java
@@ -3,13 +3,32 @@ package com.example.project;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.TimePicker;
+import androidx.appcompat.widget.Toolbar;
 
-public class ReminderAddActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class ReminderAddActivity extends AppCompatActivity implements
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener{
+
+    private int mYear, mMonth, mHour, mMinute, mDay;
+    private Calendar cal;
+    private Toolbar toolbar;
+    private TextView setTitle, setDate, setTime, setRepeat, repeatNo, repeatType, setAddress, setContact;
+    private int year, month, hour, minute, day;
+    private String Date, Time, Title;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -17,20 +36,72 @@ public class ReminderAddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_setting);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        TextView setDate = (TextView) findViewById(R.id.set_date);
+        toolbar = findViewById(R.id.toolbar);
+        setTitle = findViewById(R.id.title);
+        setDate = findViewById(R.id.set_date);
+        setTime = findViewById(R.id.set_time);
+        setRepeat = findViewById(R.id.set_repeat);
+        repeatNo = findViewById(R.id.set_repeat_no);
+        repeatType = findViewById(R.id.set_repeat_type);
+        setAddress = findViewById(R.id.set_address);
+        setContact = findViewById(R.id.set_contact);
 
-        setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("setReminder_title");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+        setSupportActionBar(toolbar);
 
-    }
+        cal = Calendar.getInstance();
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        minute = cal.get(Calendar.MINUTE);
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH) + 1;
+        day = cal.get(Calendar.DATE);
 
-    private void setSupportActionBar(Toolbar toolbar) {
+        Date = day + "/" + month + "/" + year;
+        Time = hour + ":" + minute;
 
+        setTitle.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Title = s.toString().trim();
+                setTitle.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 
     public void set_Date(View view) {
+    }
+
+    public void set_Time(View view) {
+    }
+
+    public void set_Address(View view) {
+    }
+
+    public void set_Contact(View view) {
+    }
+
+    public void setRepeatNo(View view) {
+    }
+
+    public void selectRepeatType(View view) {
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
     }
 }
